e22683745daf363e86080712b365b3d4580bf51b
poi <daichi.403.horie@gmail.com>
2026-09-17 01:35:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "todo-calendar-board-v95";
+const CACHE_NAME = "todo-calendar-board-v96";
 const ASSETS = [
   "./",
   "./index.html",
@@ -22,6 +22,10 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match("./index.html")))
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
   );
 });
